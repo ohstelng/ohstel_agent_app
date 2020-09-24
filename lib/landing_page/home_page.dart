27d1006b542b9696c33ab2979e2c6697195e3a17
cykic <cyb3rcykic@gmail.com>
2020-09-24 17:41:59 -0400

    login design homepage

--- lib/landing_page/home_page.dart
+++ lib/landing_page/home_page.dart
@@ -1,15 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ohostel_hostel_agent_app/auth/methods/auth_methods.dart';
 import 'package:ohostel_hostel_agent_app/food/pages/food_home.dart';
-
-import 'package:ohostel_hostel_agent_app/widgets/custom_button.dart';
-import 'package:ohostel_hostel_agent_app/widgets/styles.dart' as Styles;
-import 'package:ohostel_hostel_agent_app/market_place/pages/market_home_page.dart';
-
-
-import '../hostel_booking/hoste_home_page.dart';
+import 'package:ohostel_hostel_agent_app/hostel_booking/hoste_home_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,123 +13,70 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey[200],
-        leading: Padding(padding: EdgeInsets.all(6),child: Image.asset("asset/ohstel.png"),),
-        elevation: 5,
-        title: Text('Management Console',style: TextStyle(color: Colors.black),),
+        title: Text('Home'),
         centerTitle: true,
       ),
       body: Container(
-        color: Colors.white,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Spacer(
-              flex: 1,
-            ),
-            RichText(
-              text: TextSpan(
-                  text: " Welcome, ",
+            Center(
+              child: FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => HostelHomePage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Hostel Agent App',
                   style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: " Ohstel Agent",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold,fontSize: 24, color: (Styles.themePrimary)),
-                    ),
-                  ]),
-            ),
-            Spacer(
-              flex: 1,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 8),
-                  height: 135,
-                  width: 162,
-                  decoration: Styles.boxDec,
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => HostelHomePage(),
-                        ),
-                      );
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset("asset/chostel.svg"),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          'Hostel Agent App',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        )
-                      ],
-                    ),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 8),
-                  height: 135,
-                  width: 162,
-                  decoration: Styles.boxDec,
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => FoodHomePage(),
-                        ),
-                      );
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset("asset/cfood.svg"),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          'Food Agent App',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        )
-                      ],
+              ),
+            ),
+            Center(
+              child: FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => FoodHomePage(),
                     ),
-
-              
+                  );
+                },
+                child: Text(
+                  'Food Agent App',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ],
+              ),
             ),
-            Spacer(
-              flex: 1,
-            ),
-            LongButton(
-                color: Styles.themePrimary,
-                labelColor: Colors.white,
-                label: "Log Out",
+            Center(
+              child: FlatButton(
+                color: Colors.green,
                 onPressed: () async {
                   await AuthService().signOut();
-                }),
-            Spacer(
-              flex: 3,
-            )
+                },
+                child: Text(
+                  'Log Out',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
